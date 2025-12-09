b8e6a9d236002f9142dba3b60d7332db0cf15668
Nicholo Dacanay <nicholodacanayz@gmail.com>
2025-12-09 18:40:06 +0800

    identify and suggest

--- organizer.py
+++ organizer.py
@@ -1,3 +1,21 @@
 import os
 import shutil
 
+base_dir = r"C:/Users/USERNAME/Downloads"
+
+extensions = {
+    ".exe": "Executables",
+    ".jpg": "Images",
+    ".png": "Images",
+    ".pdf": "Documents",
+    ".zip": "Archives"
+}
+
+for filename in os.listdir(base_dir):
+    file_name, file_extension = os.path.splitext(filename)
+
+    if file_extension in extensions:
+        folder_name = extensions[file_extension]
+
+        print(f"Found {filename} -> Needs to go to {folder_name}")
+    
